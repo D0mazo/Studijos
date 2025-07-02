2a8a87b5d7257836afb4440f174922e9eec89481
D0mazo <dominykas.simanavicius@proton.me>
2025-07-02 12:32:07 +0300

    not working

--- MngSystem/vite.config.js
+++ MngSystem/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    loader: { '.js': 'jsx' },
+    loader: { '.js': 'jsx', '.jsx': 'jsx' },
   },
 });
